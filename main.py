9c7a7c79a2273066fdba9618770d36275b4cc9b6
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-02 10:33:51 +0100

    resources check

--- main.py
+++ main.py
@@ -6,31 +6,35 @@ money = 0
 report ="""
 """
 
-
-
-
-#TODO prompt user for input
-machine_prompt = (input('What would you like? (espresso/latte/cappuccino):')).strip().lower()
-
-
 #TODO Turn off coffee machine
 coffee_machine_on = True
 
-if machine_prompt == "off":
-    coffee_machine_on = False
-
-if machine_prompt == "report":
-    resources["money"] = money
-    for item in resources:
-        name = item.title()
-        amount = resources[item]
-
-        if name == "Coffee":
-            report += f"{name}: {amount}g\n"
-        elif name == "Money":
-            report += f"{name}: ${amount}\n"
-        else:
-            report += f"{name}: {amount}ml\n"
+while coffee_machine_on == True:
 
 
-    print(report)
+    #TODO prompt user for input
+    machine_prompt = (input('What would you like? (espresso/latte/cappuccino):')).strip().lower()
+
+    if machine_prompt == "off":
+        coffee_machine_on = False
+
+    if machine_prompt == "report":
+        resources["money"] = money
+        for item in resources:
+            name = item.title()
+            amount = resources[item]
+
+            if name == "Coffee":
+                report += f"{name}: {amount}g\n"
+            elif name == "Money":
+                report += f"{name}: ${amount}\n"
+            else:
+                report += f"{name}: {amount}ml\n"
+
+        print(report)
+
+    #TODO check resource sufficiency
+    if machine_prompt == "espresso" or machine_prompt == "latte" or machine_prompt == "cappuccino":
+        for ingredient in menu[machine_prompt]["ingredients"]:
+            if menu[machine_prompt]["ingredients"][ingredient] > resources[ingredient]:
+                print(f"Sorry there is not enough {ingredient.lower()}")
